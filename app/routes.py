31662852063d06a9eca1edc93681a36bdfd0bea3
Nasaruddin <nasaruddin@skorpoints.com>
2021-03-04 16:38:04 +0700

    add pagination

--- app/routes.py
+++ app/routes.py
@@ -26,9 +26,18 @@ def index():
         db.session.commit()
         flash('Your post is now live!.')
         return redirect(url_for('index'))
-    user = {'username': 'Nasa'}
-    posts = current_user.followed_posts().all()
-    return render_template('index.html',  title="Home", posts=posts, form=form)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template(
+        'index.html',  title="Home", posts=posts.items,
+        next_url=next_url, prev_url=prev_url, form=form
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -140,5 +149,15 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('explore', page=posts.next_num) if \
+        posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if \
+        posts.has_prev else None
+    return render_template(
+        'index.html', title='Explore', posts=posts.items,
+        next_url=next_url, prev_url=prev_url
+    )
